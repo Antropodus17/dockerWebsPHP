2fa958d85bc38b341ede32e6a641cda184b045d0
Antropodus <a23sergiopn>
2024-10-09 18:44:42 +0200

    avanzando en el login y posterior

--- proyectoPHP/auth/login.php
+++ proyectoPHP/auth/login.php
@@ -1,26 +1,104 @@
-<?php 
+<?php
+
 declare(strict_types=1);
 
 //IMPORTS
-require_once("../utils/ayudaFormulario.php");
+require_once("../utils/validador.php");
 
+//VARIABLES
+global $errorMessage;
+
+//SIMULATION OF DATABASE
+global $usersDatabase;
+$usersDatabase = [
+    "root" => "abc123.",
+    "user" => "prueba"
+];
 
 
 //OBJECTS
-global $formulary;
-$formulary=new AyudaFormulario();
+global $validator;
+$validator = new Validador();
+
+//FUNCTIONS
+
+/**
+ * 
+ */
+function loginSucces()
+{
+    global $validator;
+    session_start([
+        "user" => $validator->clean($_POST["user"]),
+        "password" => $validator->clean($_POST["paswd"])
+    ]);
+    $redirect=$_COOKIE["noUser"];
+    header("Location: $redirect");
+}
+
+/**
+ * 
+ */
+function validatePassword()
+{
+    global $usersDatabase;
+    global $validator;
+    if (!$validator->comprobarValorArray($usersDatabase, $validator->clean($_POST["user"]), $validator->clean($_POST["paswd"]))) {
+        throw new Exception("Wrong password");
+    }
+}
+
+/**
+ * 
+ */
+function validateUser()
+{
+    global $usersDatabase;
+    global $validator;
+    if (!isset($validator->clean($_POST["user"]))) {
+        throw new Exception("Wrong user");
+    }
+}
+
+/**
+ * 
+ */
+function validateLogin()
+{
+    $loginSucces = true;
+
+    //VALIDATIONS
+    try {
+        validateUser();
+        validatePassword();
+    } catch (Exception $e) { //ERROR IN VALIDATION
+        $errorMessage = $e->getMessage();
+        $loginSucces = false;
+    }
+    if ($loginSucces) { //VALIDATION OKEY?
+        loginSucces();
+    }
+}
+
+//REVISION
+
+if ($_SERVER["HTTP_REFERER"] == $_SERVER["PHP_SELF"]) {
+}
+
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
 </head>
+
 <body>
     <form action=<?php echo $_SERVER["PHP_SELF"]; ?> method="post">
-        
+
         <label for="user">User: </label>
         <input type="text" name="user" id="iUser"><br>
         <label for="paswd">Password</label>
@@ -28,4 +106,5 @@ $formulary=new AyudaFormulario();
         <input type="submit" value="Iniciar Sesion">
     </form>
 </body>
+
 </html>
--- proyectoPHP/src/energyCalculator.php
+++ proyectoPHP/src/energyCalculator.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 if (!isset($_SESSION["user"])) {//COMPROBATE IF SESSION IS STARTED
+    setcookie("noUser",$_SERVER["PHP_SELF"],time()+60*60,"/");
     header("Location: http://localhost/proyectoPHP/auth/login.php");
     exit;
 }
